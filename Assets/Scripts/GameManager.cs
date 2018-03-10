057b94fba4d64a2a2139983f6369b1f933c1837f
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-03-10 05:38:28 -0500

    Setting up code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour {
 
+    public Tilemap UIHighlights;
+
+    public enum GameState
+    {
+        BaseState,
+        UnitSelected,
+        Targeting
+    }
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+            }
+        }
+
 	}
 }
