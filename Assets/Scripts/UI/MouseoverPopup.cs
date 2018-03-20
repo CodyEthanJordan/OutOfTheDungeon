17abf0f696fa1c1e5fcfed2cdc27a95a7442007b
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-03-19 23:58:01 -0400

    More UI stuff

--- Assets/Scripts/UI/MouseoverPopup.cs
+++ Assets/Scripts/UI/MouseoverPopup.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 namespace Assets.Scripts.UI
 {
-    class MouseoverPopup
+    class MouseoverPopup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        public GameObject DetailsBox;
+        public String Details = "";
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            DetailsBox.SetActive(true);
+            DetailsBox.GetComponentInChildren<Text>().text = Details;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            DetailsBox.SetActive(false);
+        }
     }
 }
