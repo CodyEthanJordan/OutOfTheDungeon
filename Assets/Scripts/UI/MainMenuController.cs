5be123c7d8fdfd95f71c2ccf49b6967a5fbfe1f2
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-03-20 22:44:32 -0400

    Start button don't work

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        private PlayableDirector pd;
+        private void Awake()
+        {
+            pd = GetComponent<PlayableDirector>();
+        }
+
+        public void StartGame()
+        {
+            pd.Play();
+            StartCoroutine(SceneTransition());
+        }
+
+        private System.Collections.IEnumerator SceneTransition()
+        {
+            yield return new WaitForSeconds((float)pd.duration);
+            SceneManager.LoadScene("DungeonScene", LoadSceneMode.Single);
+        }
+    }
+}
